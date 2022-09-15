8051839464655ad14c18716f6ec3fdfce411336c
Seth Jones <sjone270@uncc.edu>
2022-09-15 16:52:43 -0400

    Worked on odd_Range

--- PythonBasics1/pythonBasics1.py
+++ PythonBasics1/pythonBasics1.py
@@ -8,9 +8,13 @@
 # Define a function odd_range(num1, num2) that takes a starting number (num1) and an ending number (num2)
 # and returns all odd numbers as an array between num1 (inclusive) and num2 (exclusive)
 def odd_range(num1, num2):
-  # YOUR CODE HERE
+  oddArray = []
 
-  return
+
+  for x in range (num1, num2):
+    if (x % 2 != 0):
+      oddArray.append(x)
+  return oddArray
 
 # Part B. has_lower_case
 # Define a function has_lower_case(s) that takes a string s
