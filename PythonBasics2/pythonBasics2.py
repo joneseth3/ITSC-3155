d9ed790f6a0e38759cc581b76b9af4c615df47aa
Seth Jones <sjone270@uncc.edu>
2022-10-06 11:06:12 -0400

    PythonBasics 2 - Updated Methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -15,12 +15,12 @@ from re import A
 
 
 def count_threes(n):
-	n = list(str(n)) 
+	n = list(str(n)) #turns string into list 
 	dic ={}
 
-	dic[3] = dic[6] = dic[9] = 0
+	dic[3] = dic[6] = dic[9] = 0  #since we only are checking single digit numbers, I set the 3/div numbers to 0 in the dict
 	
-	for i in n:
+	for i in n:  #runs through the list an pulls number to dict that are 3/div and counts the occurences 
 		j = int(i)
 		if j % 3 == 0 and j != 0:
 			dic[j] = dic[j] + 1
@@ -28,7 +28,7 @@ def count_threes(n):
 
 	max = -1
 	index = -1
-	for i,j in dic.items():
+	for i,j in dic.items():    #checks the biggest numbers and then returns for answer 
 		if j > max:
 			max = j
 			index = i
@@ -40,12 +40,12 @@ def count_threes(n):
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
-  s = list(s)
+  s = list(s)   #turns string into list 
   list_length = len(s)
   count = 1
 
   dic = {}
-  for i in range(0,list_length -1):
+  for i in range(0, list_length -1):   #runs through the list and pulls chars into dic and counts occurences 
     if(s[i] != s[i+1]):
       if((s[i] in dic) and dic[s[i]] > count):
         continue
@@ -57,7 +57,7 @@ def longest_consecutive_repeating_char(s):
 
   dic[s[list_length -1]] = count
 
-  max = -1
+  max = -1    #checks the chars that occur the most and then returns for answer
   for i,j in dic.items():
     if j > max:
       max = j
