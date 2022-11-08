e5aa09b7995a8767a1cb0b7d93bdd5d27c7aa4b6
Seth Jones <sjone270@uncc.edu>
2022-11-08 14:06:51 -0500

    flask app #5 -edit note form displays existing note

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -76,12 +76,17 @@ def new_note():
         return render_template('new.html', user = a_user)
 
 
+
+
 @app.route('/notes/edit/<note_id>')
 def update_note(note_id):
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
 
     my_note = db.session.query(Note).filter_by(id=note_id).one()
 
     return render_template('new.html', note=my_note, user=a_user)
+
+
+
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
